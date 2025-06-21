119394a934d2f031562662c7bcd3aa3dc3fb525f
jram <jram6269@gmail.com>
2025-06-21 13:18:56 +0530

    Basic Authentication Done

--- backend/server.py
+++ backend/server.py
@@ -1,29 +1,29 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from motor.motor_asyncio import AsyncIOMotorClient
+from pydantic import BaseModel
 
 
 app = FastAPI()
 app.add_middleware(CORSMiddleware,
     allow_origins=["*"],
     allow_credentials = True,
-    allow_requests = True,
+    allow_headers=["*"],
     allow_methods=["*"])
 
 cluster = AsyncIOMotorClient("mongodb+srv://devlabs07:0rsKIrtlWd1h0Fn6@cluster0.0onzi5k.mongodb.net/")
 database = cluster["Authentication"]
 collection = database["login"]
 
-structure = {
-    "usermail": str,
-    "password": str
-}
+class Login(BaseModel):
+    usermail: str
+    password: str
+
 
 @app.post("/login")
-async def login(user: structure):
-    usermail = user["usermail"]
-    password = user["password"]
-    
+async def login(user: Login):
+    usermail = user.usermail
+    password = user.password
     if not usermail or not password:
         return {"error": "Email and password are required."}
     user_data = await collection.find_one({"usermail": usermail})
